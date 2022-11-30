61de5b1274b4b564e7d6dd5a07f73f762c0e500e
loaferOvO <251492435@qq.com>
2022-11-30 11:02:04 +0800

    添加int转data函数

--- data.h
+++ data.h
@@ -6,6 +6,19 @@ typedef struct Data
     int symbol;//符号
 }Data;
 
+Data InttoData(int x)
+{
+    Data rlt;
+    rlt.number.init();
+    rlt.symbol=x<0?-1:1;
+    while(x){
+        rlt.number.push_back(x%10);
+        x/=10;
+    }
+    rlt.point=0;
+    return rlt;
+}
+
 //常量(自然常量)
 const Data E={getE(),50,1};
 //常量(圆周率)
